f825b8eae49c84312ad6c2730b07375463f5d4a4
Paul <Forgames.app2022@gmail.com>
2024-05-27 10:04:27 +0800

    Add MD file

--- GreeksForGreeksChallenge/src/TwoDimensionalArray/SimpleSearching.java
+++ GreeksForGreeksChallenge/src/TwoDimensionalArray/SimpleSearching.java
@@ -53,13 +53,19 @@ public class SimpleSearching {
         }
         if (tempVal==searchKey){
             if (tempVal>=101 && tempVal<=112){
-                System.out.println("The"+ searchKey+"code you are searching for is ["+array1[row][col]+"]");
+                System.out.println("The ["+searchKey+"] code you are searching for is ["+array1[row][col]+"]");
                 System.out.println("You will see it in Cabinet [1] at "+level+shelves);
             }else  if (tempVal>=201 && tempVal<=212){
-                System.out.println("The"+ searchKey+"code you are searching for is [Margarine]");
+                System.out.println("The ["+ searchKey+"] code you are searching for is ["+array1[row][col]+"]");
                 System.out.println("You will see it in Cabinet [2] at "+level+shelves);
             }
-        }
+            header3();
+            hiddenCabinet1();
+            header4();
+            hiddenCabinet2();
+        }else{
+            System.out.println("Invalid Input!");
+    }
 }
 
     static void header1() {
@@ -81,7 +87,6 @@ public class SimpleSearching {
             System.out.println();
         }
     }
-
     static void header2() {
         System.out.println("---------------------------------------------------------------");
         System.out.println("Cabinet #:2\tShelves[2]\tShelves[2]\tShelves[3]\tShelves[4]");
@@ -94,6 +99,48 @@ public class SimpleSearching {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j <= array.length + 1; j++) {
                 System.out.print(array[i][j] + "  \t\t");
+            }
+            System.out.println();
+        }
+    }
+    static void header3(){
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("\tCODE LIST OF GROCERY ITEMS FOUND INSIDE THE CABINET");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Cabinet #:1\t   Shelves[1]\t    Shelves[2]\t    Shelves[3]\t    Shelves[4]");
+        System.out.println("-------------------------------------------------------------------------------");
+    }
+    static void hiddenCabinet1(){
+        int cols = 5;
+        int rows = 3;
+
+        String array[][]= {{"Level[3]", "Ampalaya","Beef\t","Milk\t","Coffee"},
+            {"Level[2]", "Pechay", "Pork\t","Butter","Creamer"},
+            {"Level[1]", "Onion", "\tChicken","Cheese", "Sugar"}};
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <cols; j++) {
+                System.out.print(array[i][j]+ "  \t\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static  void header4() {
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Cabinet #:2\t   Shelves[2]\t    Shelves[2]\t    Shelves[3]\t    Shelves[4]");
+        System.out.println("-------------------------------------------------------------------------------");
+    }
+    static  void hiddenCabinet2(){
+        int cols = 5;
+        int rows = 3;
+
+        String array[][]= {{"Level[3]","Lettuce\t","Sausage\t", "Yogurt\t", "Honey"},
+            {"Level[2]", "Broccoli", "Hot Dogs", "Margarine", "Cereal"},
+            {"Level[1]", "Carrots\t", "Bacon\t", "Sour Cream", "Green Tea"}};
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <cols; j++) {
+                System.out.print(array[i][j]+ "\t\t");
             }
             System.out.println();
         }
